fd3e8124d75d56cfe00f5b39279d2f99e9143c0a
sureyaf <sureya.farah@georgbrown.ca>
2020-09-29 22:52:33 -0400

    upload from local

---
+++ src/main/java/ca/gbc/petclinic/PetClinicApplication.java
@@ -0,0 +1,13 @@
+package ca.gbc.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //CONFIGURATION to spring boot
+public class PetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicApplication.class, args);
+    }
+
+}
